136acb11153d93108987625465be67973e23894a
EnesArslan8 <enes8arslan@gmail.com>
2023-09-04 14:35:45 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import { useState } from "react";
+import "./App.css";
+function App() {
+  const month = [
+    {
+      id: 1,
+      value: "Ocak",
+      label: "Ocak",
+    },
+    {
+      id: 2,
+      value: "Şubat",
+      label: "Şubat",
+    },
+    {
+      id: 3,
+      value: "Mart",
+      label: "Mart",
+    },
+    {
+      id: 4,
+      value: "Nisan",
+      label: "Nisan",
+    },
+    {
+      id: 5,
+      value: "Mayıs",
+      label: "Mayıs",
+    },
+    {
+      id: 6,
+      value: "Haziran",
+      label: "Haziran",
+    },
+    {
+      id: 7,
+      value: "Temmuz",
+      label: "Temmuz",
+    },
+    {
+      id: 8,
+      value: "Ağustos",
+      label: "Ağustos",
+    },
+    {
+      id: 9,
+      value: "Eylül",
+      label: "Eylül",
+    },
+    {
+      id: 10,
+      value: "Ekim",
+      label: "Ekim",
+    },
+    {
+      id: 11,
+      value: "Kasım",
+      label: "Kasım",
+    },
+    {
+      id: 12,
+      value: "Aralık",
+      label: "Aralık",
+    },
+  ];
+  const year = [
+    {
+      id: 1,
+      value: "2023",
+      label: "2023",
+    },
+    {
+      id: 2,
+      value: "2024",
+      label: "2024",
+    },
+    {
+      id: 3,
+      value: "2025",
+      label: "2025",
+    },
+    {
+      id: 4,
+      value: "2026",
+      label: "2026",
+    },
+    {
+      id: 5,
+      value: "2027",
+      label: "2027",
+    },
+    {
+      id: 6,
+      value: "2028",
+      label: "2028",
+    },
+    {
+      id: 7,
+      value: "2029",
+      label: "2029",
+    },
+    {
+      id: 8,
+      value: "2030",
+      label: "2030",
+    },
+    {
+      id: 9,
+      value: "2031",
+      label: "2031",
+    },
+    {
+      id: 10,
+      value: "2032",
+      label: "2032",
+    },
+    {
+      id: 11,
+      value: "2033",
+      label: "2033",
+    },
+    {
+      id: 12,
+      value: "2034",
+      label: "2034",
+    },
+  ];
+  
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
